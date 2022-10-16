12aab86d21d41d6bce73e61d31a686cedcd4b32c
Kevin Santiago <sonicthe1111@gmail.com>
2022-10-16 03:05:53 -0500

    Integración de Menu mobile con una animación en la navbar

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', () => {desktopMenu.classList.toggle('inactive');} );
+burgerMenu.addEventListener('click', () => {mobileMenu.classList.toggle('activoM');} );
 
 console.log('Ketchup');
